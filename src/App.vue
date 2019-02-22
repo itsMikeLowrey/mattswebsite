2a437f8cbf31894462e0329a5d2597ef8f708667
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-21 22:23:45 -0800

    Added background image

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-title>Matt Murphy</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Cars</v-btn>
+          <v-btn flat>motorcycles</v-btn>
+          <v-btn flat>Space Talk</v-btn>
+        </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   },
   data () {
     return {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <HelloWorld />
+<div class="back">
+</div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
 export default {
   components: {
-    HelloWorld
+
   }
 }
 </script>
+<style scoped>
+
+.back {
+  /* The image used */
+  background-image: url("https://images.pexels.com/photos/1388278/pexels-photo-1388278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+
+  /* Full height */
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
