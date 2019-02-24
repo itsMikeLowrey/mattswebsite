668f1d779574c559382a55de3d2f75494416e555
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-24 14:54:03 -0800

    Working But Not Looking Great

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
 <div class="back">
   <v-flex class="hidden-sm-and-down intro" xs12 style="position:relative;height:100vh;">
-           <v-flex class="title" xs8 offset-xs2 pa-4>
-           <div class="display-4 white--text text-xs-center " >
+           <v-flex class="title toolbar" xs12>
+           <div class="display-1 white--text text-xs-center " >
              Matthew Murphy
            </div>
-           <br>
-           <div class="text-xs-center">
+           </v-flex>
+           <v-flex class="buttons toolbar" xs12>
+             <div class="text-xs-center">
 
-             <v-btn flat class="buttonOpt">Cars</v-btn>
-             <v-btn flat class="buttonOpt">Motorcycles</v-btn>
-             <v-btn flat class="buttonOpt">Space Talk</v-btn>
-           </div>
-         </v-flex>
+               <v-btn flat class="buttonOpt">Cars</v-btn>
+               <v-btn flat class="buttonOpt">Space Talk</v-btn>
+               <v-btn flat class="buttonOpt"><router-link to="/all">All Post</router-link></v-btn>
+             </div>
+           </v-flex>
+
   </v-flex>
 </div>
 </template>
@@ -25,7 +27,10 @@ export default {
 }
 </script>
 <style scoped>
-
+a {
+  text-decoration: none;
+  color: white;
+}
 .back {
   /* The image used */
   background-image: url('~@/assets/websiteBackground.jpg');
@@ -35,17 +40,16 @@ export default {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+
+}
+.toolbar{
+  background-color: rgb(105,105,105);
 }
 .title{
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgb(169,169,169,.9);
+height: 6vh;
   /* border-radius: 3%; */
 }
-.buttonOpt {
+.buttons {
   color:  black;
   font-size: 1.4rem;
 }
